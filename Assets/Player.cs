1b2ef8b6019c86c979e15921818fd2c9e27ec23b
alhahay0603 <mishaalex35@mail.ru>
2026-03-07 20:08:39 +0800

    dasdasdsadsadasdasdsadadasdasd

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -8,12 +9,28 @@ public class Player : MonoBehaviour
     public Transform attackPoint;
     public int coins;
 
+    //Обьект, отвечающий за проигрывание звука
+    public AudioSource audioSource;
+
+    //Звуковой файл, содержащий звук урона
+    public AudioClip damageSound;
+
+
     //Здоровье игрока
     private int health = 10;
 
     //Метод понижающий здоровье игрока
     public void TakeDamage(int damage) {
         health -= damage;
+        if(health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
         print("Здоровье игрока:" + health);
 
 
@@ -35,12 +52,14 @@ public class Player : MonoBehaviour
         }
     }
 
-    
+
     //Метод для сбора монет
     public void CollectCoins()
     {
         coins += 1;
         print("Количество монет" + coins);
     }
+
+
     
 }
